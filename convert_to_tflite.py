e89642bf7e8c6df01b3db2a0eb0486c0dfae1720
thangtran <thangtran480@gmail.com>
2019-08-27 23:06:12 +0700

    refator code

--- convert_to_tflite.py
+++ convert_to_tflite.py
@@ -1,5 +1,17 @@
 import tensorflow as tf
 
-converter = tf.lite.TFLiteConverter.from_keras_model_file("models/hairnet_matting_30.hdf5")
-tflite_model = converter.convert()
-open("models/converted_model_hairnet.tflite", "wb").write(tflite_model)
+if __name__ == '__main__':
+    # https://www.tensorflow.org/lite/performance/post_training_quantization#float16_quantization_of_weights
+
+    tf.enable_eager_execution()
+
+    PATH_MODEL = "models/hair.h5"
+    PATH_MODEL_TFLITE = "models/model_hairnet.tflite"
+
+    converter = tf.lite.TFLiteConverter.from_keras_model_file(PATH_MODEL)
+
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.target_spec.supported_types = [tf.lite.constants.FLOAT16]
+
+    tflite_model = converter.convert()
+    open(PATH_MODEL_TFLITE, "wb").write(tflite_model)
